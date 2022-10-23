770910ec6b924de04081c7e02321eaef3d67c168
garredondop <garredondop@uni.pe>
2022-10-23 16:07:58 -0500

    inicio

--- sockets/controller.js
+++ sockets/controller.js
@@ -3,6 +3,12 @@
 
 const socketController = (socket) => {
     
+    console.log('Cliente conectado', socket.id );
+
+    socket.on('disconnect', () => {
+        console.log('Cliente desconectado', socket.id );
+    });
+
     socket.on('enviar-mensaje', ( payload, callback ) => {
         
         const id = 123456789;
