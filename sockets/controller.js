666b734b32398536367577f6db123487527b2c1c
unknown <Administrator@LIM-H7NR1F3.usersad.everis.int>
2022-10-23 22:00:56 -0500

    clean the code

--- sockets/controller.js
+++ sockets/controller.js
@@ -1,14 +1,5 @@
-
-
-
 const socketController = (socket) => {
     
-    console.log('Cliente conectado', socket.id );
-
-    socket.on('disconnect', () => {
-        console.log('Cliente desconectado', socket.id );
-    });
-
     socket.on('enviar-mensaje', ( payload, callback ) => {
         
         const id = 123456789;
